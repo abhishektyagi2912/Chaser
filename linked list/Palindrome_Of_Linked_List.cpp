18a5c242b73002512bb42c5ecd575c2eec459576
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-01-22 21:37:12 +0530

    Palindrome

---
+++ linked list/Palindrome_Of_Linked_List.cpp
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/palindrome-linked-list/  best approach
+
+// Approach 2: duplicates a array 
+
+// https://practice.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/1
