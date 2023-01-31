7c53b0e2da7f8546db8bce1ed2253bf64f176be4
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-02-01 00:09:46 +0530

    reverse string using stack

--- Stack/Reverse_A_String.cpp
+++ Stack/Reverse_A_String.cpp
@@ -8,13 +8,14 @@ int main()
     string str = "Abhishek";
     stack<char> s;
 
+    // acces the string words
     for (auto i : str)
     {
         s.push(i);
     }
 
     string ans = "";
-
+    // create new string and push the words in the string till then stack is not empty
     while (!s.empty())
     {
         ans.push_back(s.top());
