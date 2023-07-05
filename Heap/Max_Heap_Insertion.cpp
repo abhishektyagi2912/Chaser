c8600e3acd74579b0ebd2fa5d20c4e7e7c4be3c9
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-07-06 01:34:23 +0530

    reading

--- Heap/Max_Heap_Insertion.cpp
+++ Heap/Max_Heap_Insertion.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
 using namespace std;
-class Heap{
-    public:
+class Heap
+{
+public:
     int arr[100];
     int size;
-    Heap(){
-        arr[0]=-1;
-        size=0;
+    Heap()
+    {
+        arr[0] = -1;
+        size = 0;
     }
 
-    void insert(int val){
-        size = size+1;
-        int index= size;
-        arr[index]=val;
+    void insert(int val)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
 
-        while(index>1){
-            int parent = index/2;
-            
-            if(arr[parent]<arr[index]){
-                swap(arr[parent],arr[index]);
-                index= parent;
+        while (index > 1)
+        {
+            int parent = index / 2;
+
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
             }
-            else{
-                return ;
+            else
+            {
+                return;
             }
         }
     }
-    void print(){
-        for(int i =1;i<=size;i++){
-            cout<<arr[i]<<" ";
-        }cout<<endl;
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
-
 };
 
 int main()
